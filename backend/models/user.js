b7795f52dc02ef64ea9d5d68e27b11351ff1588a
Zoren Villareal <zoren.villareal@gmail.com>
2024-01-16 16:28:17 -0800

    launched

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,22 +4,21 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-
     static associate({ Comment }) {
       User.hasMany(Comment, { as: 'author', foreignKey: 'author_id' })
     }
-
   };
   User.init({
     userId: {
       type: DataTypes.SMALLINT,
       primaryKey: true,
       autoIncrement: true
-
     },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING
+    email: DataTypes.STRING,
+    passwordDigest: DataTypes.STRING
   }, {
     sequelize,
     underscored: true,
